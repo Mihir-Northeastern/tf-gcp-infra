597513e8e8d7eed21028901846d6e558dee6923d
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-12 17:32:52 -0500

    created vpc

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable"project_id" {
+  description = "Project Id - GCP"
+  type = string
+}
+
+variable"vpc_name" {
+  description = "VPC Name - GCP"
+  type = string
+}
+
+variable"region" {
+  description = "Region - GCP"
+  type = string
+}
+
+variable"auto_create_subnetworks" {
+  description = "Auto Create Subnetworks - GCP"
+  type = bool
+}
+
+variable"routing_mode" {
+  description = "Routing Mode - GCP"
+  type = string
+}
