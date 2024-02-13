8af02f5f54bad5fb5021cb17d66e8768a6ad97b1
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-13 12:32:38 -0500

    added one more vpc and yml file

--- variables.tf
+++ variables.tf
@@ -7,6 +7,10 @@ variable"vpc_name" {
   description = "VPC Name - GCP"
   type = string
 }
+variable"vpc_name_1" {
+  description = "VPC Name 1 - GCP"
+  type = string
+}
 
 variable"region" {
   description = "Region - GCP"
