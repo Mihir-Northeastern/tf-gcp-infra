851b2547c90f94712947317ec04b344f17d0ad3c
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-14 20:38:02 -0500

    created subnets and added route

--- variables.tf
+++ variables.tf
@@ -1,28 +1,39 @@
-variable"project_id" {
+variable "project_id" {
   description = "Project Id - GCP"
-  type = string
+  type        = string
 }
 
-variable"vpc_name" {
+variable "vpc_name" {
   description = "VPC Name - GCP"
-  type = string
-}
-variable"vpc_name_1" {
-  description = "VPC Name 1 - GCP"
-  type = string
+  type        = string
 }
 
-variable"region" {
+variable "region" {
   description = "Region - GCP"
-  type = string
+  type        = string
 }
 
-variable"auto_create_subnetworks" {
+variable "auto_create_subnetworks" {
   description = "Auto Create Subnetworks - GCP"
-  type = bool
+  type        = bool
 }
 
-variable"routing_mode" {
+variable "routing_mode" {
   description = "Routing Mode - GCP"
-  type = string
+  type        = string
+}
+
+variable "subnet_1" {
+  description = "Subnet 1 - GCP"
+  type        = string
+}
+
+variable "subnet_2" {
+  description = "Subnet  2 - GCP"
+  type        = string
+}
+
+variable "route" {
+  description = "Router - GCP"
+  type        = string
 }
