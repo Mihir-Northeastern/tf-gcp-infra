e1640603c013c87ab71a3ad56aed9a19d0e4c01a
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-14 20:38:02 -0500

    created subnets and added route

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
-#   credentials = file(var.credentials)    
-  project     = var.project_id
-  region      = var.region
+  #   credentials = file(var.credentials)    
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_compute_network" "vpc_name" {
@@ -10,8 +10,23 @@ resource "google_compute_network" "vpc_name" {
   routing_mode            = var.routing_mode
 }
 
-resource "google_compute_network" "vpc_name_1" {
-  name                    = var.vpc_name_1
-  auto_create_subnetworks = var.auto_create_subnetworks
-  routing_mode            = var.routing_mode
+resource "google_compute_subnetwork" "subnet_1" {
+  name          = var.subnet_1
+  ip_cidr_range = "10.0.0.0/24"
+  region       = var.region
+  network      = google_compute_network.vpc_name.self_link
+}
+
+resource "google_compute_subnetwork" "subnet_2" {
+  name          = var.subnet_2
+  ip_cidr_range = "10.0.1.0/24"
+  region       = var.region
+  network      = google_compute_network.vpc_name.self_link
+}
+
+resource "google_compute_route" "route" {
+  name                  = var.route
+  network               = google_compute_network.vpc_name.self_link
+  dest_range            = "0.0.0.0/0"
+  next_hop_gateway = "default-internet-gateway"
 }
