147c799d84de38ca562828f2a20d63580583507b
Mihir Makwana <makwana.mi@northeastern.edu>
2024-02-22 20:51:35 -0500

    aaded images

--- variables.tf
+++ variables.tf
@@ -13,6 +13,11 @@ variable "region" {
   type        = string
 }
 
+variable "delete_default_routes_on_create" {
+  description = "Delete Default Routes On Create - GCP"
+  type        = bool
+}
+
 variable "auto_create_subnetworks" {
   description = "Auto Create Subnetworks - GCP"
   type        = bool
@@ -23,6 +28,10 @@ variable "routing_mode" {
   type        = string
 }
 
+variable "dest_range" {
+  description = "Destination Range - GCP"
+  type        = string
+}
 variable "subnet_1" {
   description = "Subnet 1 - GCP"
   type        = string
@@ -33,7 +42,27 @@ variable "subnet_2" {
   type        = string
 }
 
+variable "ip_cidr_range_1" {
+  description = "IP CIDR Range 1 webapp- GCP"
+  type        = string
+}
+
+variable "ip_cidr_range_2" {
+  description = "IP CIDR Range 2 db- GCP"
+  type        = string
+}
 variable "route" {
   description = "Router - GCP"
   type        = string
+}
+
+variable "next_hop_gateway" {
+  description = "Next Hop Gateway - GCP"
+  type        = string
+}
+
+variable "address" {
+  description = "Address - GCP"
+  type        = string
+  
 }
